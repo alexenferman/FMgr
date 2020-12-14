579c12d9c06e064b28c09b52a09311452117f3ce
alexenferman <alextud0132@gmail.com>
2020-12-13 22:07:04 -0500

    initial

---
+++ main.js
@@ -0,0 +1,19 @@
+function folder() {
+ var x = document.getElementsByClassName("file")
+	for(var i = 0; i < x.length; i++)
+    {x[i].style.display="none";}
+}
+
+
+function generate() {
+var MotoE2020 = {
+    name : "Moto E 2020",
+    link : "Moto E 2020/Custom ROMS"
+}  
+  
+var galaxyj16 = {name : "galaxyj16", link : "galaxyj16Custom ROMS"}
+
+var galaxyj16_device = {n22ame : "Moto E 2020", linker : "Moto E 2020Custom ROMS"}
+
+console.table(galaxyj16_device);
+}
